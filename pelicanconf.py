1421adc38ab360ac97f68fc741e8ba9ed4aaf20e
Darrick <darrick.code@gmail.com>
2019-02-18 21:17:40 -0800

    perserve .git in output repo

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Darrick Sturgeon'
 SITENAME = 'S Vision'
-SITEURL = 'http://dasturge.github.io'
+SITEURL = ''
 SITELOGO = SITEURL + '/images/profile.jpg'
 
 PATH = 'content'
@@ -21,15 +21,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'))
+LINKS = (('My Github', 'https://github.com/dasturge'),
+        ('DCAN-labs Github', 'https://github.com/DCAN-Labs'),
+        ('Python.org', 'http://python.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/dasturge'),
+          ('another link', '#'))
 
 DEFAULT_PAGINATION = 10
+
+OUTPUT_RETENTION = ['.git']
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True

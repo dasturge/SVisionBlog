6fc0e2b31292ed8570c41f74b8e2a426a693697e
Darrick <darrick.code@gmail.com>
2019-02-18 17:23:59 -0800

    major updates to image math. Edit settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Darrick Sturgeon'
 SITENAME = 'S Vision'
-SITEURL = ''
+SITEURL = 'http://dasturge.github.io'
+SITELOGO = SITEURL + '/images/profile.jpg'
 
 PATH = 'content'
 
@@ -22,8 +23,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -32,12 +32,14 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
 
-# THEME = "/home/leonhard/pelican-themes/Flex"
+IGNORE_FILES = [".ipynb_checkpoints"]
+
+THEME = "/home/euler/pelican-themes/Flex"
 
